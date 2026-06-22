f0dccfad04d22e6fa12d8196a135192d439febff
Anne0617 <495711690@qq.com>
2026-06-22 13:59:11 +0800

    add backend/learning/admin.py

---
+++ backend/learning/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import StarCheckinScore
+
+@admin.register(StarCheckinScore)
+class StarCheckinAdmin(admin.ModelAdmin):
+    list_display = ("user", "day_number", "score", "hp_left", "completed_at")
+    list_filter = ("day_number", "completed_at")
+    search_fields = ("user__username", "user__last_name")
+    date_hierarchy = "completed_at"
